70d4e9103d2be15ca31512db4d77fd9f6eba1bf7
Ritheesh padavala <mailtoritheesh@gmail.com>
2023-09-12 21:34:45 +0530

    swapping of 2 numbers added

---
+++ swap.py
@@ -0,0 +1,16 @@
+# The Traditional way of swapping
+a = 5
+b = 6
+temp = a
+a = b
+b = temp
+print(a)
+print(b)
+# Swapping using Operators
+a = 5
+b = 6
+a = a+b
+b = a-b
+a = a-b
+print(a)
+print(b)
